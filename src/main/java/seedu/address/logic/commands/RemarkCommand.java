0b4f4ee67072c075a6d23bec3266e921f1fd9e57
yiwen101 <e0968880@u.nus.edu>
2023-09-30 21:55:58 +0800

    abstract remark into class, and added to ui

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -4,6 +4,7 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Remark;
 
 public class RemarkCommand extends Command {
     public static final String COMMAND_WORD = "remark";
@@ -20,13 +21,13 @@ public class RemarkCommand extends Command {
     public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
 
     private final Index index;
-    private final String remark;
+    private final Remark remark;
 
     /**
      * @param index of the person in the filtered person list to edit the remark
      * @param remark of the person to be updated to
      */
-    public RemarkCommand(Index index, String remark) {
+    public RemarkCommand(Index index, Remark remark) {
         requireNonNull(index);
         requireNonNull(remark);
         this.index = index;
